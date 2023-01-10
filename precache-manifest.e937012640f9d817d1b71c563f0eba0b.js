9800b9878ba1a3a256ba9057ee48a768338372a7
ThunderSmoker <pitlawararadhya@gmail.com>
2023-01-11 04:25:20 +0530

    Updates

---
+++ precache-manifest.e937012640f9d817d1b71c563f0eba0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f5b8817192bd1f3b92a",
+    "url": "/yuvarang23/static/css/main.5a047b34.chunk.css"
+  },
+  {
+    "revision": "4f5b8817192bd1f3b92a",
+    "url": "/yuvarang23/static/js/main.4f5b8817.chunk.js"
+  },
+  {
+    "revision": "aea9f4a8eb578ca1e1c2",
+    "url": "/yuvarang23/static/js/1.aea9f4a8.chunk.js"
+  },
+  {
+    "revision": "9c61ff915b45adce483a",
+    "url": "/yuvarang23/static/js/runtime~main.9c61ff91.js"
+  },
+  {
+    "revision": "af3df276a49dff687594afe4776769e8",
+    "url": "/yuvarang23/index.html"
+  }
+];
