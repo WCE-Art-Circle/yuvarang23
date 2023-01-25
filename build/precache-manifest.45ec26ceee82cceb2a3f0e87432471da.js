80c2746c4e4696ead4dc591b7597ec232a3635b5
ThunderSmoker <pitlawararadhya@gmail.com>
2023-01-25 11:01:25 +0530

    build

---
+++ build/precache-manifest.45ec26ceee82cceb2a3f0e87432471da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1018f5846c34fad9f5ad",
+    "url": "/yuvarang23/static/css/main.0ffbd842.chunk.css"
+  },
+  {
+    "revision": "1018f5846c34fad9f5ad",
+    "url": "/yuvarang23/static/js/main.1018f584.chunk.js"
+  },
+  {
+    "revision": "f2f9f8d62f17b9bd698a",
+    "url": "/yuvarang23/static/css/1.e0eecccf.chunk.css"
+  },
+  {
+    "revision": "f2f9f8d62f17b9bd698a",
+    "url": "/yuvarang23/static/js/1.f2f9f8d6.chunk.js"
+  },
+  {
+    "revision": "9c61ff915b45adce483a",
+    "url": "/yuvarang23/static/js/runtime~main.9c61ff91.js"
+  },
+  {
+    "revision": "36fec06af766e505ac0bc2474824ad2e",
+    "url": "/yuvarang23/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/yuvarang23/precache-manifest.51e9082bb301f922afdbdc606064f07f.js"
+  "/yuvarang23/precache-manifest.45ec26ceee82cceb2a3f0e87432471da.js"
 );
 
 workbox.clientsClaim();
